51b211eacc623afac28788ab4ff9cb596ca3b480
nawomaiy <naomisya0@gmail.com>
2025-03-21 12:20:29 +0700

    hadeh vercel rewel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-  root: '.', // Harus menunjuk ke lokasi index.html
-  build: {
-    outDir: 'dist', // Pastikan ini ada
-  }
-});
+    root: 'public',
+    build: {
+      outDir: '../dist',
+    }
+  });
+  
