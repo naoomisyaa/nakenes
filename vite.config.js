2e87e65f5f11c437a12d6f56afd53979b879bcf2
nawomaiy <naomisya0@gmail.com>
2025-03-21 12:29:58 +0700

    hadeh vercel rewel

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css', 
-                'resources/js/app.js'
-            ],
-            refresh: [
-                'resources/views/**/*.blade.php',  
-                'routes/**/*.php'                  
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // Pastikan output sesuai Laravel
+        emptyOutDir: true,
+    },
 });
